49d29f6c9992328baae2a6294c17a9f7c163aaed
Italy25 <kirryx2@gmail.com>
2021-02-17 11:27:58 +0900

    update and play a little

--- web/gotest.go
+++ web/gotest.go
@@ -1,12 +1,36 @@
 package main
+
 import (
-  "net/http"
-  "github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
+type Member struct{
+  leader string
+  sub string
+}
+
+type Contents struct{
+  header string
+  content string
+}
+
+type Document struct{
+  name string
+  member Member
+  contents Contents
+}
+
+func (d Document) dict() string{
+  return d.contents.content
+}
 func main() {
   router := gin.Default()
+  var member = Member{"hirata","wada"}
+  var contents = Contents{"This is Test","testing is important."}
+  var doc = Document{"go-test",member,contents}
   router.GET("/", func(c *gin.Context) {
-    c.String(http.StatusOK, "Hello World!!")
+    c.String(http.StatusOK, doc.dict())
   })
   router.Run(":8080")
 }
